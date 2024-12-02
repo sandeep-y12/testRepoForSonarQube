6089f310991d8cf70a3d4bed5b4910ed8b99c83f
sandeepy2 <sandeep.y@ebrandz.us>
2024-12-02 18:06:12 +0530

    added the test code for sonar qube

---
+++ app/Calculator.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+class Calculator {
+    /**
+     * Create a new class instance.
+     */
+    public function __construct() {
+        //
+    }
+
+    public function addTwoNumbers($num1, $num2) {
+        return $num1 - $num2;
+    }
+}
